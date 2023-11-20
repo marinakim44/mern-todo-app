3c65ee87717573cd30b049e67958bfb132bc5149
marinakim44 <marina.kim.mk90@gmail.com>
2023-11-20 07:07:24 +0000

    first commit from youtube live

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
-import { useState } from "react";
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { Button, Form } from "react-bootstrap";
 
 function App() {
   // local data state
   const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [updatedTask, setUpdatedTask] = useState({});
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/get-all-tasks")
+      .then((res) => {
+        console.log(res.data);
+        setTasks(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   // function
   const handleChange = (e) => {
@@ -14,24 +28,117 @@ function App() {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    axios.post('http://localhost:3001/save-task', {
-      taskName: task
-    })
-    .then(res => console.log(res.data))
-    .catch(err => console.log(err))
-  }
+    axios
+      .post("http://localhost:3001/save-task", {
+        taskName: task,
+      })
+      .then((res) => {
+        console.log(res.data);
+        if (res.data === "Task saved") {
+          console.log("task added successfully...");
+          setTask("");
+          window.location.reload();
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const deleteTask = (id) => {
+    axios
+      .delete(`http://localhost:3001/delete-task/${id}`)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data === "Task deleted") {
+          window.location.reload();
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleChangeUpdate = (e) => {
+    const { name, value } = e.target;
+
+    setUpdatedTask((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const updateTask = (id) => {
+    console.log("Updating task");
+
+    axios
+      .put(`http://localhost:3001/update-task/${id}`, {
+        taskName: updatedTask[id],
+      })
+      .then((res) => {
+        console.log(res.data);
+        setIsUpdate(false);
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div>
-      <div>
+      <div style={{ padding: 10 }}>
         <h1>ToDo App</h1>
         <p style={{ fontStyle: "italic" }}>
           Date: {new Date().toLocaleDateString()}
         </p>
-        <div style={{display:'flex', flexDirection:'column', width:'50%'}}>
-          <input style={{marginBottom: 10}} placeholder="Type here..." onChange={handleChange} />
-          <button onClick={handleSubmit}>Add Task</button>
+        <div
+          style={{ display: "flex", flexDirection: "column", width: "100%" }}
+        >
+          <Form>
+            <Form.Group>
+              <Form.Control
+                style={{ marginBottom: 10 }}
+                placeholder="Type here..."
+                onChange={handleChange}
+                value={task}
+              />
+            </Form.Group>
+          </Form>
+
+          <Button onClick={handleSubmit} style={{ marginBottom: 20 }}>
+            Add Task
+          </Button>
         </div>
+
+        {tasks.map((elem, index) => {
+          return (
+            <div
+              style={{
+                borderRadius: "7px",
+                padding: 10,
+                margin: "10px 0px",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+              key={index}
+            >
+              <Form.Control
+                onClick={() => setIsUpdate(true)}
+                onChange={handleChangeUpdate}
+                value={
+                  updatedTask[elem._id] ? updatedTask[elem._id] : elem.taskName
+                }
+                name={elem._id}
+                style={{ marginRight: 5 }}
+              />
+              <Button
+                variant={isUpdate ? "outline-primary" : "success"}
+                onClick={() =>
+                  isUpdate ? updateTask(elem._id) : deleteTask(elem._id)
+                }
+              >
+                {isUpdate ? "Update" : "Done"}
+              </Button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const taskSchema = new mongoose.Schema({
   taskName: String,
 });
 
-const Task = new mongoose.model("Item", taskSchema);
+const Task = new mongoose.model("Task", taskSchema);
 
 // API routes
 app.get("/", (req, res) => {
@@ -46,12 +46,41 @@ app.post("/save-task", (req, res) => {
     .catch((err) => console.log(err));
 });
 
-// other api endpoints
-app.delete("/delete-task/:id", (req, res) => {
-  console.log(req.body);
+app.get("/get-all-tasks", (req, res) => {
+  console.log("Reading all tasks...");
+  Task.find()
+    .then((docs) => {
+      console.log(docs);
+      res.json(docs);
+    })
+    .catch((err) => console.log(err));
 });
+
 app.delete("/delete-task/:id", (req, res) => {
-  console.log(req.body);
+  console.log(req.params);
+
+  Task.deleteOne({ _id: req.params.id })
+    .then((doc) => {
+      console.log(doc);
+      res.send("Task deleted");
+    })
+    .catch((err) => console.log(err));
+});
+
+app.put("/update-task/:id", (req, res) => {
+  console.log(req.params, req.body);
+
+  Task.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      taskName: req.body.taskName,
+    }
+  )
+    .then((doc) => {
+      console.log(doc);
+      res.send("Task updated");
+    })
+    .catch((err) => console.log(err));
 });
 
 // server launch
